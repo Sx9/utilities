b2e2d2500b517c8f1967b2186d99a50ad817027d
Sx9 <news@sandipan.com>
2021-09-14 20:05:56 -0400

    Made the conflict resolver recursive and also move backups to root folder

--- dropbox_manipulation/Conflict File Resolver.py
+++ dropbox_manipulation/Conflict File Resolver.py
@@ -2,38 +2,62 @@
 
 import os, sys, pickle, os.path
 from pathlib import Path
-from datetime import datetime
+# from os import path
+# from datetime import datetime
+
+# TODO: Make this recursive and find other instances ...
 
 strProblemPath = "D:\\Dropbox (Sandipan.com)\\Development\\Site Builder\\Mobirise\\experiment03-sandipan.com"
+strProblemPath = "D:\\Dropbox (Sandipan.com)\\Development"
+strProblemPath = "D:\\Dropbox (Sandipan.com)\\website-accounts"
+strBackupFolderName = "back"
+binAskForConfirm = True
 
-lstFiles = os.listdir(strProblemPath)
+def fnFixFilesInFolder(strFolderPath):
 
-print("Files found in directory: [%s]" % lstFiles)
+    lstFiles = os.listdir(strFolderPath)
+    print("Files found in directory [%s]: [%s]" % (strFolderPath, lstFiles))
 
-for strFile in lstFiles:
+    for strFile in lstFiles:
 
-    if "conflicted copy" in strFile:
+        strFilePath = os.path.join(strFolderPath, strFile)
 
-        lstFilePieces = strFile.split("(")
-        strFileExt = strFile.split(".")[1]
-        lstFileOriginal = lstFilePieces[0].strip()
-        strOrigFile = lstFileOriginal + "." + strFileExt
-        objOrigFile = Path(strOrigFile)
-        print("File: [%s] v/s [%s]" % (strFile, strOrigFile))
+        if os.path.isdir(strFilePath):
+            fnFixFilesInFolder(strFilePath)
 
-        if strOrigFile in lstFiles:
-
-            print("File [%s] should be replaced by file [%s]" % (strFile, strOrigFile))
-
-            strDecision = input("Replace (Y/N) [N]")
-
-            if strDecision.lower() == "y":
-
-                if True:
-                    os.rename(os.path.join(strProblemPath, strOrigFile), os.path.join(strProblemPath, strOrigFile) + ".bak")
-                    os.rename(os.path.join(strProblemPath, strFile), os.path.join(strProblemPath, strOrigFile))
-                    print("Swapped")
+        else:
+            if "conflicted copy" in strFile:
+                lstFilePieces = strFile.split("(")
+                lstFileExt = strFile.split(".")
+                if len(lstFileExt) > 1:
+                    strFileExt = "." + lstFileExt[1]
                 else:
-                    print("Failed")
-                    continue
+                    strFileExt = ""
+                lstFileOriginal = lstFilePieces[0].strip()
+                strOrigFile = lstFileOriginal + strFileExt
+                objOrigFile = Path(strOrigFile)
+                print("File: [%s] v/s [%s]" % (strFile, strOrigFile))
 
+                if strOrigFile in lstFiles:
+                    print("File [%s] should be replaced by file [%s]" % (strFile, strOrigFile))
+
+                    if binAskForConfirm:
+                        strDecision = input("Replace (Y/N) [N]")
+                    else:
+                        strDecision = "y"
+
+                    if strDecision.lower() == "x":
+                        break
+
+                    if strDecision.lower() == "y":
+                        try:
+                            os.replace(os.path.join(strFolderPath, strOrigFile), os.path.join(strBackupFolderPath, strOrigFile) + ".bak")
+                            os.rename(os.path.join(strFolderPath, strFile), os.path.join(strFolderPath, strOrigFile))
+                            print("Swapped")
+                        except:
+                            print("Failed")
+
+strBackupFolderPath = os.path.join(strProblemPath, strBackupFolderName)
+if not os.path.exists(strBackupFolderPath):
+    os.mkdir(strBackupFolderPath)
+fnFixFilesInFolder(strProblemPath)
